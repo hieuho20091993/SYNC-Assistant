831311855139ff9492b942c0f3f9b7f0f85cb501
Hieu Ho <hho@ucsd.edu>
2020-10-26 15:01:04 +0700

    Sync Brain

---
+++ hieu.py
@@ -0,0 +1,10 @@
+print ("hieu")
+
+user = "hello"
+
+if user == "hello":
+	sync = "Hello"
+elif user == "How are you?":
+	sync = "I am fine, thank you."
+elif user == "":
+	sync = "I cannot hear you, please speak louder"
